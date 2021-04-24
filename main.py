ee34f8c67c2391416056aee41841b60b8dd73ab7
João Marcelo S. Vaz <jmsvaz@gmail.com>
2021-04-24 14:33:24 -0300

    added option to update more than one date at once ( still no cache of file)

--- main.py
+++ main.py
@@ -14,34 +14,44 @@ if not gc.loadFile():
     exit(' # GnuCash file not available!')
 
 fundsFileMng = FundsFileMng(settings.cvm_funds_url)
-if not fundsFileMng.loadFile(settings.date):
-    exit(' # CVM file not available!')
 
 stockQuotes = StockQuotes(settings.b3_urlBase, settings.b3_fileNameBase, settings.app_files_dir)
-if not stockQuotes.loadFile(settings.date):
-    exit(' # B3 file not available!')
 
 brazilianCurrencyGuid = gc.getBrasilianCurrencyGuid()
 commodities = gc.getCommodities()
 newPriceList = []
 
-print('Searching prices for ' + settings.date + ':')
-for c in commodities:
-    result = None
-    if (c[1] in settings.CVM_Funds_commodities):
-        result = fundsFileMng.getQuotesByCnpjDate(c[2], settings.date)
+for date in settings.dates:
+    print('Searching prices for ' + date + ':')
 
-    if (c[1] in settings.B3_commodities):
-        result = stockQuotes.getPriceByDate(c[2], settings.date)
-
-    if result != None:
-        denom = int(10 ** numberOfDigits(result))
-        value = int(result * denom)
-        print('  Found price for ' + c[3])
-        newPriceList.append(GnuCashPrice(c[0],c[3],brazilianCurrencyGuid,settings.date, denom, value))
+    if not fundsFileMng.loadFile(date):
+        print(' # CVM file not available!')
+    else:
+        for c in commodities:
+            result = None
+            if (c[1] in settings.CVM_Funds_commodities):
+                result = fundsFileMng.getQuotesByCnpjDate(c[2], date)
+            if result != None:
+                denom = int(10 ** numberOfDigits(result))
+                value = int(result * denom)
+                print('  Found price for ' + c[3])
+                newPriceList.append(GnuCashPrice(c[0],c[3],brazilianCurrencyGuid,date, denom, value))
+                
+    if not stockQuotes.loadFile(date):
+            print(' # B3 file not available!')
+    else:
+        for c in commodities:
+            result = None
+            if (c[1] in settings.B3_commodities):
+                result = stockQuotes.getPriceByDate(c[2], date)
+            if result != None:
+                denom = int(10 ** numberOfDigits(result))
+                value = int(result * denom)
+                print('  Found price for ' + c[3])
+                newPriceList.append(GnuCashPrice(c[0],c[3],brazilianCurrencyGuid,date, denom, value))
 
 if len(newPriceList) > 0:
     print('Updating GnuCash:')
     gc.savePrices(newPriceList)
 else:
-    print('No price found for ' + settings.date)
+    print('No prices found!')
